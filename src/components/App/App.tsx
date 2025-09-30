0498648177b3892f5f632b21a9dd4de392ff8dfa
Stanislav Hanchev <stanislav@smoothr.de>
2025-09-30 13:43:51 +0300

    fix VoteType using

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,18 @@
 import css from './App.module.css';
+
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
 
-// import type { VoteType } from '../../types/votes';
-import type { Votes } from '../../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 
 import { useState } from 'react'
 
 function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
-  const handleVote = (key: keyof Votes) => {
+  const handleVote = (key: VoteType) => {
     setVotes({
       ...votes,
       [key]: votes[key] + 1,
